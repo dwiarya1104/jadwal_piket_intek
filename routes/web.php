9d0385bf1ec6fbb9f43c6a7946d03286f01fefdd
Dwi Arya Putra <dwiarya1104@gmail.com>
2022-04-21 14:29:56 +0700

    route edit schedule from user

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,8 @@ Route::group(['prefix' => 'schedule'], function(){
     Route::get('/', [ScheduleController::class, 'index'])->name('schedule.index');
     Route::get('/addSchedule', [ScheduleController::class, 'create'])->name('schedule.create');
     Route::get('/{id}/editSchedule', [ScheduleController::class, 'edit'])->name('schedule.edit');
+    Route::get('/{id}/editScheduleUser', [ScheduleController::class, 'editUser'])->name('schedule.editUser');
+    Route::put('/{id}/updateScheduleUser', [ScheduleController::class, 'updateUser'])->name('schedule.updateUser');
     Route::post('/storeSchedule', [ScheduleController::class, 'store'])->name('schedule.store');
     Route::get('/deleteSchedule/{id}', [ScheduleController::class, 'destroy'])->name('schedule.delete');
 
