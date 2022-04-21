6f2681d9c26ae12f469006e6b6c6374bbff7196f
Dwi Arya Putra <dwiarya1104@gmail.com>
2022-04-21 14:36:24 +0700

    delete something

--- resources/views/schedule/create.blade.php
+++ resources/views/schedule/create.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.master')
 
 @section('main')
-
-
-
 <div class="container">
 <div class="card shadow m-0 p-0">
     <div class="card-header py-3">
@@ -24,19 +21,13 @@
 
             <div class="form-group">
                 <label for="#" class="font-weight-bold h6">Description</label>
-                <textarea name="task_description" id="" class="form-control" cols="10" rows="2" placeholder="Description"></textarea>
-                {{-- <input type="text" class="form-control" name="task_description" id="task_description" placeholder="Description"> --}}
+                <input type="text" class="form-control" name="task_description" id="task_description" placeholder="Description">
             </div>
 
             <label for="#" class="font-weight-bold h6">AssignTo</label>
             <div class="input-group">
                     {!! Form::select('user_id', $users, null, ['class' => 'form-control select', 'placeholder' => '-- Choose Office Boys --', 'id' => 'user_id', 'required']) !!}
-
-                    {{-- @foreach ($data as $ob)
-                    <option value="{{ $ob->id }}">{{$ob->user->name}}</option>
-                    @endforeach --}}
               </div>
-
         <div class="form-group">
             <label for="#" class="font-weight-bold h6 mt-3">Start Time</label>
             <input type="datetime-local" class="form-control" id="startTime" name="start_time" id="start_time" placeholder="Start Time">
@@ -46,32 +37,6 @@
             <label for="end_time" class="font-weight-bold h6">End Time</label>
             <input type="datetime-local" class="form-control" id="endTime" name="end_time" id="end_time" placeholder="End Time">
         </div>
-
-        <label for="#" class="font-weight-bold h6">Status</label>
-        <div class="input-group">
-            {{-- {!! Form::select('status', $status, null, ['class' => 'form-control select', 'placeholder' => '-- Choose Office Boys --', 'id' => 'status', 'required']) !!} --}}
-            <select class="form-control" name="status">
-                <option value="">--Select Status--</option>
-                <option value="Completed">  Completed</option>
-                <option value="Incompleted">Incompleted</option>
-                <option value="On Progress">On Progress</option>
-            </select>
-            {{-- <select name="status" id="status" class="form-control">
-                @foreach ($data as $ob)
-                <option value="{{ $ob->id }}">{{$ob->status}}</option>
-                @endforeach
-            </select> --}}
-          </div>
-
-          <label for="#" class="font-weight-bold h6 mt-3">Bukti</label>
-          <div class="input-group">
-            <div class="input-group-prepend">
-            </div>
-            <div class="custom-file">
-              <input type="file" name="upload_bukti" class="custom-file-input" id="inputGroupFile01">
-              <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-            </div>
-          </div>
           <div class="form-group">
               <button type="submit" class="btn btn-primary bt-sm mt-4">Add</button>
               <a href="#" type="submit" class="btn btn-primary bt-sm">Cancel</a>
