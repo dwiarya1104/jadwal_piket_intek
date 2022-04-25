437eb1a1ae840a90c0b243193045651eceedb451
Dwi Arya Putra <dwiarya1104@gmail.com>
2022-04-25 16:07:39 +0700

    create remember token

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
             'username' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('password'),
+            'remember_token' => Str::random(60),
         ]);
 
         $admin->assignRole('admin');
@@ -26,6 +27,7 @@ class UserSeeder extends Seeder
             'username' => 'user',
             'email' => 'user@gmail.com',
             'password' => bcrypt('password'),
+            'remember_token' => Str::random(60),
         ]);
 
         $user->assignRole('user');
