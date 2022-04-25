3aee8bd680432ed9f3c50af7e09010eeded65fa1
Dwi Arya Putra <dwiarya1104@gmail.com>
2022-04-25 10:33:22 +0700

    api login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use app\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'Auth\LoginController@login');
