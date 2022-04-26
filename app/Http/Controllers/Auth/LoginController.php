4c655088f10f0b14696240b0d1e5618af49a68b6
Dwi Arya Putra <dwiarya1104@gmail.com>
2022-04-26 08:49:30 +0700

    status

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -77,9 +77,9 @@ public function apiLogin(Request $request)
                 'username' => $user->username,
                 'email' => $user->email,
             );
-            return response()->json(['success' => 200,'message'=> 'Login Successfuly', 'data' => $data,]);
+            return response()->json(['status' => 'success','message'=> 'Login Successfuly', 'data' => $data,]);
         } else {
-            return response()->json(['error' => 401, 'message'=> 'Login Failed']);
+            return response()->json(['status' => 'error', 'message'=> 'Login Failed']);
         }
         }
 }
